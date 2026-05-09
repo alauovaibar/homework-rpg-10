06257b06f77be52a745a4f4094b4160973caec5d
alauovaibar <alauovajbar@gmail.com>
2026-05-09 19:20:17 +0500

    HW 10

--- src/com/narxoz/rpg/council/CouncilEngine.java
+++ src/com/narxoz/rpg/council/CouncilEngine.java
@@ -1,19 +1,41 @@
 package com.narxoz.rpg.council;
 
 import com.narxoz.rpg.combatant.Hero;
+import com.narxoz.rpg.guild.GuildHall;
 import com.narxoz.rpg.guild.GuildMediator;
+import com.narxoz.rpg.quest.Quest;
+import com.narxoz.rpg.quest.QuestIterator;
 import com.narxoz.rpg.quest.QuestLog;
+import com.narxoz.rpg.quest.QuestPriority;
+
 import java.util.List;
 
 /**
  * Orchestrates a planning session that uses both Iterator and Mediator.
  */
 public class CouncilEngine {
-
     public CouncilRunResult runCouncil(List<Hero> party, QuestLog questLog, GuildMediator hall) {
-        // TODO: walk questLog with at least 2 different iterators,
-        //       dispatch coordinating messages through hall for each quest,
-        //       and return counters (questsTraversed, messagesRouted, membersNotified).
-        return new CouncilRunResult(0, 0, 0);
+        int questsTraversed = 0;
+
+        QuestIterator ordered = questLog.getOrderedIterator();
+        while (ordered.hasNext()) {
+            Quest q = ordered.next();
+            questsTraversed++;
+            hall.dispatch("QUEST_REPORT", null, "New quest: " + q.getTitle());
+        }
+
+        QuestIterator priority = questLog.getPriorityIterator(QuestPriority.HIGH);
+        while (priority.hasNext()) {
+            Quest q = priority.next();
+            questsTraversed++;
+            hall.dispatch("COMMAND", null, "URGENT: " + q.getTitle());
+        }
+
+        GuildHall gh = (GuildHall) hall;
+        return new CouncilRunResult(
+                questsTraversed,
+                gh.getMessagesRouted(),
+                gh.getNotificationsSent()
+        );
     }
 }
